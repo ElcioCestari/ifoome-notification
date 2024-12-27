fb4c74e368630957e69de8e512715be1bd7dd4be
elcio <elciotaira@gmail.com>
2024-12-26 20:19:16 -0400

    refactor: change send msg to completabe future

--- src/main/java/com/elciocestari/ifoomenotification/service/MessageConsumerService.java
+++ src/main/java/com/elciocestari/ifoomenotification/service/MessageConsumerService.java
@@ -1,10 +1,8 @@
 package com.elciocestari.ifoomenotification.service;
 
-import com.elciocestari.ifoomenotification.entity.Email;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
